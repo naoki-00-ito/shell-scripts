a5030248374e20fccfddfe5263565cc2a609145a
Naoki Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-01-18 20:33:27 +0900

    [Add] loop-copy.sh

---
+++ loop-copy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# 引数チェック
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <filepath> <loop_count>"
+  exit 1
+fi
+
+filepath=$1
+loop_count=$2
+number=1
+
+# 拡張子取得
+extension="${filepath##*.}"
+
+# パス取得
+directory=$(dirname "$filepath")
+
+while [ $number -le $loop_count ]; do
+  filename=$(basename "$filepath" ".$extension")
+  cp "$filepath" "$directory/${filename}_${number}.$extension"
+  number=$((number + 1))
+done
